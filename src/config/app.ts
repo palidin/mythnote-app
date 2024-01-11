55d76eb7cf4484178f44ec680bf61d863471101d
palidin <palidin@foxmail.com>
2024-01-11 16:34:32 +0800

    支持线上环境

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,8 +1,19 @@
+const devUrl = 'http://api-myth-note.test.cn';
+const onlineUrl = '/api';
+
+const serverUrl = isDevelopment() ? devUrl : onlineUrl;
+
 export const appConfig = {
   // serverUrl: 'http://note.palidin.me',
-  serverUrl: 'http://api-myth-note.test.cn',
+  serverUrl: serverUrl,
   onlineImageUrl: 'https://image.palidin.io',
   offlineImageUrl: 'http://image.palidin.me',
 }
 
 export const TAG_TRASH = '//trash';
+
+
+function isDevelopment() {
+  let href = window.location.href;
+  return href.includes('localhost') || href.includes('127.0.0.1') || href.includes('192.168.') || href.endsWith('test.cn')
+}
