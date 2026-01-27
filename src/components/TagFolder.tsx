cc8997b9f2913653cb88839303214df4f1b15e11
palidin <palidin@foxmail.com>
2026-01-27 16:25:21 +0800

    移除无用的依赖

--- src/components/TagFolder.tsx
+++ src/components/TagFolder.tsx
@@ -6,9 +6,20 @@ import {showConfirmModal, showInputModal} from "../utils/MessageUtils";
 import {myAgent} from "../agent/agentType";
 import clsx from "clsx";
 
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faChevronRight} from "@fortawesome/free-solid-svg-icons/faChevronRight";
 
+const ChevronRightIcon = ({ className, ...props }) => (
+  <svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    viewBox="0 0 320 512" 
+    fill="currentColor"
+    className={className}
+    {...props}
+    width="1em"
+    height="1em"
+  >
+    <path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/>
+  </svg>
+);
 
 export function TagFolder({folders, onTagClick, keys = [], keyword = ''}) {
 
@@ -101,8 +112,7 @@ export function TagFolder({folders, onTagClick, keys = [], keyword = ''}) {
                 </div>
                 {hasChildren && (
                   <div className="flex items-center ml-2">
-                    <FontAwesomeIcon
-                      icon={faChevronRight}
+                    <ChevronRightIcon
                       className={clsx(
                         'text-xs text-white/75 transition-transform duration-200',
                         {
